29c312ed396539956de92c36508c1db5f5e8b0cf
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2014-04-22 17:47:58 +0300

    s

---
+++ src/main/java/com/arvue/apps/newest/Main.java
@@ -0,0 +1,11 @@
+package com.arvue.apps.newest;
+
+import org.vaadin.mideaas.MideaasComponent;
+
+public class Main extends MideaasComponent {
+
+    @Override
+    public void attach() {
+        super.attach();
+    }
+}
